5448b61fef3f5168ff9e3f984ee44524626eb825
VarushSiva <varushsivalingam@gmail.com>
2024-12-19 19:34:18 -0500

    Add inital book functions

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Varush's Library");
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
