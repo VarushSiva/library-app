67d0597b3e2834c412ecf252e7f3bec4cd7027b2
VarushSiva <varushsivalingam@gmail.com>
2024-12-25 04:16:30 -0500

    Add Basic layout outlines + 1 Book

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+function displayBooks(bookList) {
+  for (let i = 0; i < bookList.length; i++) {
+    let container = document.querySelector(".container");
+    let cards = document.createElement("div");
+    container.appendChild(cards);
+    cards.className("book");
+  }
+}
